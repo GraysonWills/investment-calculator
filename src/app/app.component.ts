db5df918c4ca8b8a7ee69fccc813e3327a843938
GraysonWills <146792117+GraysonWills@users.noreply.github.com>
2025-05-11 17:04:38 -0400

    Investment Calculator Initial commit
    Creating all necessary components:
    1. graph
    2. user input
    3. user table
    4. header
    
    No code has been provided yet, since I need to upgrade
    my version of angular to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { UserInputComponent } from './components/user-input/user-input.component';
+import { UserTableComponent } from './components/user-table/user-table.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UserInputComponent, UserTableComponent, GraphComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'investment-calculator';
+  
 }
